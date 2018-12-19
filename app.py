c721be630e7b8b55f3e0e94a5b2cd6f76563609b
c890224xc <43979116+c890224xc@users.noreply.github.com>
2018-12-19 15:09:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def find_bookls(kw):
     return ans
 
 def loadPMJson():
-    with urllib.request.urlopen("http://opendata2.epa.gov.tw/AQX.json") as url:
+    with urllib.request.urlopen("http://opendata2.epa.gov.tw/AQI.json") as url:
         data = json.loads(url.read().decode())
         for ele in data:
             pm_site[ele['SiteName']] = ele['PM2.5']
@@ -154,7 +154,7 @@ def get_search_engine(search_thing, result_num=4):
     return result
 if __name__ == "__main__":
     # load PM2.5 records
-    loadPMJson()
+    #loadPMJson()
 	
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
